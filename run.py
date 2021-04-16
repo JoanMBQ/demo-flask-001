64c0138d29f2f84b0a463b05ffa6a7dc8b5f6d46
JoanMBQ <timo_2604@hotmail.com>
2021-04-16 18:11:30 -0500

    Demo Flask

--- run.py
+++ run.py
@@ -9,12 +9,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return "<h1>Hola mundo con Flask</h1>"
+    return "<h1>Hola mundo con Flask</h1><h2>Demo flask</h2><h3>by JoanMBQ</h3>"
 
 @app.route('/suma')
 def suma():
-    resultado = 5 + 5
-    return "<h3>5 + 5 = %d</h3>" % (resultado)
+    resultado = 523 + 4234
+    return "<h3>523 + 4234 = %d</h3>" % (resultado)
+
+@app.route('/listado')
+def listado():
+    return  "<h2>Listado de materias del itinerario basado en plataformas</h2><ul></ul><li>Plataformas Web</li><li>Plataformas Moviles</li><li>Plataformas para Juegos</li></ul>"
+
 
 if __name__ == "__main__":
-    app.run(host='127.1.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
